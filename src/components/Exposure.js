393bbb1efd46ea1734944b2ff7f9979ee2d1f98d
Patryk Kuchta <p.kuchta@se20.qmul.ac.uk>
2022-03-14 16:12:57 +0000

    minor tweaks and work on the exposure

--- src/components/Exposure.js
+++ src/components/Exposure.js
@@ -1,15 +1,13 @@
 const Exposure = ({cloudCover}) => {
-    var exposure = parseInt((-16/100)*cloudCover + 16);
+    
+    var exposure = parseInt((-104/1000)*cloudCover + 16);
     //console.log(exposure)
    return(
-      <div id="dem">
-        Recommended Exposure: f{exposure}
+      <div>
+        Recommended Exposure: f/{exposure}
       </div>
     )
   }
-  <script>
-      document.getElementById("demo").style.textAlign = "center";
-  </script>
   
   export default Exposure
   
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,22 @@
 const Map = () => {
   return (
     <div className="page map">
+      <p></p>
+      <iframe 
+        width="100%" 
+        frameborder="0" 
+        title="Common picturesque places"
+        src="https://embed.windy.com/embed2.html?lat=52.866&lon=0.842&detailLat=52.160&detailLon=-2.109&width=650&height=450&zoom=5&level=surface&overlay=clouds&product=ecmwf&menu=&message=true&marker=&calendar=now&pressure=&type=map&location=coordinates&detail=&metricWind=default&metricTemp=default&radarRange=-1"
+      >
+        <h5>
+        Common picturesque places
+        </h5>
+      </iframe>
+
     </div>
   )
+
+  //http://umap.openstreetmap.fr/en/map/anonymous-edit/731285:68qGuBwWglT37Ir5h82bDd3Y0MA
 }
 
 export default Map
